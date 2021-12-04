ddf90687b10abc1f575525725dd6654953c783d7
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-04 11:07:31 +0100

    Add function for loggin session in

---
+++ backend/login.py
@@ -0,0 +1,25 @@
+from backend.scraping import getPageSoup
+from backend.extract import extractASPData
+
+import requests
+
+
+# Function for returning a logged in user
+def loginUser(username, password, gymnasiumNumber):
+    loginURL = f"https://www.lectio.dk/lectio/{gymnasiumNumber}/login.aspx"
+
+    with requests.session() as session:
+        session.headers['user-agent'] = 'Mozilla/5.0'
+
+        print(loginURL)
+
+        loginPageSoup = getPageSoup(loginURL, session=session)
+
+        ASPData = extractASPData(loginPageSoup, "m$Content$submitbtn2")
+
+        ASPData["m$Content$username"] = username
+        ASPData["m$Content$password"] = password
+
+        session.post(loginURL, data=ASPData)
+
+    return session
