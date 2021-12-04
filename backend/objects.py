7e648ee5d7f16b8dd6dfd59db2e7732a9e08fa55
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-04 12:03:13 +0100

    Fix querying for elev id when iniating a new Elev instance

--- backend/objects.py
+++ backend/objects.py
@@ -6,4 +6,10 @@ class Elev():
     def __init__(self, session, gymnasiumNumber):
         self.session = session
         self.rootURL = f"https://www.lectio.dk/lectio/{gymnasiumNumber}/"
-        self.elevID = getElevID(session.get(rootURL).content)
+
+        self.frontPageSoup = getLoggedInPageSoup(self.rootURL, self.session)
+
+        if self.frontPageSoup:
+            self.elevID = getElevID(self.frontPageSoup)
+        else:
+            return False
