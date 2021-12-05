d37fbef3c2e0911143f1e95ac637f6de52b29b3e
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-05 08:38:21 +0100

    Add functions for handling dict of elev objects

--- mainFlask.py
+++ mainFlask.py
@@ -1,9 +1,38 @@
 from flask import Flask, render_template, Response, request, abort, redirect
 import json
 
+import uuid
+
+from Crypto.Hash import SHA256
+
 app = Flask(__name__)
 app.static_folder = "./static"
 app.template_folder = "./templates"
+
+elevDicts = {}
+
+def createInternalID(username, password):
+    return SHA256.new(f"{username}%%%{password}".encode("utf-8")).hexdigest()
+
+def removeElev(username, password):
+    internalID = createInternalID(username, password)
+
+    for elevID in list(elevDicts.keys()):
+        if elevDicts[elevID]["internalID"] == internalID:
+            del elevDicts[elevID]
+
+def addElev(username, password, elevObject):
+
+    internalID = createInternalID(username, password)
+
+    removeElev(username, password)
+
+    currentElevID = str(uuid.uuid4())
+
+    elevDicts[currentElevID] = {"elevObject" : elevObject, "internalID" : internalID}
+
+    return currentElevID
+
 
 @app.route("/")
 def redirectToGithub():
