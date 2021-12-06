15ea0ee670fd266ce2978a0fe6ae0c513af1e1d1
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-06 12:54:23 +0100

    Add function for creating and streaming CSV file

--- backend/calendar.py
+++ backend/calendar.py
@@ -1,3 +1,16 @@
+from io import StringIO
+import csv
+
+def generateCSVObject(contentList):
+    CSVObject = StringIO()
+    w = csv.writer(CSVObject)
+
+    for row in contentList:
+        w.writerow(row)
+        yield CSVObject.getvalue()
+        CSVObject.seek(0)
+        CSVObject.truncate(0)
+
 def opgaverToCalendar(opgaveDicts):
     opgaveLists = [["Subject", "Description", "Start Date", "All Day Event"]]
     for opgaveDict in opgaveDicts:
