0867418d5f006dbb6d15555728dd04765c3ba86f
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-07 17:11:33 +0100

    Add function for converting kalendar dict to list

--- backend/calendar.py
+++ backend/calendar.py
@@ -1,5 +1,6 @@
 from io import StringIO
 import csv
+from datetime import datetime
 
 def generateCSVObject(contentList):
     CSVObject = StringIO()
@@ -19,3 +20,18 @@ def opgaverToCalendar(opgaveDicts):
             opgaveLists.append([opgaveDict["Opgavetitel"], opgaveDict["Hold"], date[0], "True"]) 
 
     return opgaveLists
+
+def kalenderToCalendar(skemaDicts):
+    skemaList = [["Subject", "Start Date", "Start Time", "End Time", "Description"]]
+    for week in skemaDicts:
+        for day in skemaDicts[week]:
+            for skemaBrik in skemaDicts[week][day]["skemaBrikker"]:
+                if skemaBrik["status"] in ["Ændret!", "Uændret"]:
+                    date = {}
+                    time = {}
+                    for dateDescription in ["start", "slut"]:
+                        date[dateDescription] = skemaBrik[dateDescription].replace("-", "/").split(" ")[0]
+                        time[dateDescription] = datetime.strptime(skemaBrik[dateDescription].split(" ")[1], "%H:%M").strftime("%I:%M %p")
+
+                    skemaList.append([f"{skemaBrik['Hold']} {skemaBrik['Lokale']}", date["start"], time["start"], date["slut"], time["slut"], f"{skemaBrik['Title']} med {skemaBrik['Lærer']}" if skemaBrik["Title"] else skemaBrik["Lærer"]])
+    return skemaList
